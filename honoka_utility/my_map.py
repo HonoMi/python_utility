4ebbb9eeb041a0929cabcc3dc98d3d6507dc7223
honoka <terufumi.morishita.wp@hitachi.com>
2019-02-04 21:42:58 +0900

    [Update]

--- honoka_utility/my_map.py
+++ honoka_utility/my_map.py
@@ -29,3 +29,18 @@ class MyPool:
                 p.join()
             k += self.proc_num
         return ret
+
+
+class Test:
+
+    def fuga(self, x):
+        return x * x
+
+    def hoge(self):
+        p = MyPool(8)
+        print(p.map(self.fuga, range(10)))
+
+
+if __name__ == '__main__':
+    test = Test()
+    test.hoge()
