0d9d3f139499764e73a2063cf46be818315be904
honoka <terufumi.morishita.wp@hitachi.com>
2019-02-09 11:47:32 +0900

    [Update]

--- honoka_utility/format_markdown_itemization.py
+++ honoka_utility/format_markdown_itemization.py
@@ -29,7 +29,7 @@ def main():
     for line in lines:
         if not any([re.match(r'^\s*[-\*] .*', line), re.match(r'^\s*[0-9]\.', line)]):
             level_pre = -1
-            line_level.append((line, -1))
+            line_level.append((line, -1, None))
             continue
 
         def get_movement(pre, cur):
@@ -46,19 +46,29 @@ def main():
         if level_pre == -1:
             level_cur = 0
         else:
-            level_cur = level_pre + get_movement(int(len(spaces_pre)/4), int(len(spaces_cur)/4))
-        line_content = re.sub(r'^\s*[-\*] (.*)', r'\g<1>', line)
-        line_level.append((line_content, level_cur))
+            level_cur = level_pre + get_movement(int(len(spaces_pre) / 4), int(len(spaces_cur) / 4))
+        line_content = None
+        item_no = None
+        if re.match('^\s*[0-9]\..*', line):
+            line_content = re.sub(r'^\s*[0-9]*\. (.*)', r'\g<1>', line)
+            item_no = re.sub(r'^\s*([0-9])*\..*', r'\g<1>', line)
+        else:
+            line_content = re.sub(r'^\s*[-\*] (.*)', r'\g<1>', line)
+        line_level.append((line_content, level_cur, item_no))
         level_pre = level_cur
         spaces_pre = spaces_cur
 
-    f_out = open(args.output, 'w')
-    for line_content, level in line_level:
+    f_out = args.output if args.output == sys.stdout else open(args.output, 'w')
+    for line_content, level, item_no in line_level:
         if level < 0:
             print(line_content, file=f_out)
         else:
-            itemization_mark = '*' if level % 2 == 0 else '-'
-            print(''.join(['    ']*level) + itemization_mark + ' ' + line_content, file=f_out)
+            itemization_mark = None
+            if item_no is not None:
+                itemization_mark = item_no + '.'
+            else:
+                itemization_mark = '*' if level % 2 == 0 else '-'
+            print(''.join(['    '] * level) + itemization_mark + ' ' + line_content, file=f_out)
     f_out.close()
 
 if __name__ == "__main__":
