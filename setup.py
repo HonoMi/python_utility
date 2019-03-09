a15433f38b9442ab3d85f1bed34903eba9621790
honoka <terufumi.morishita.wp@hitachi.com>
2019-03-09 19:58:19 +0900

    [Update]

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='honoka_utility',
     version='0.0',
     description='The utility modules.',
     url='https://gitlab.rdcloud.intra.hitachi.co.jp/71389710/python_utility',
-    author='HonoHono',
+    author='Honoka',
     author_email='terufumi.morishita.wp@hitachi.com',
     license='MIT',
     install_requires=[
         'chardet',
         'coloredlogs'
-        # 'honoka_utility==0.0',    # private repositoryの依存．バージョンを指定しないと動かない．
+        # 'honoka_utility==0.0',    # private repositoryへの依存は，バージョンを指定しないと動かない．
     ],
     dependency_links=[
-        # private repositoryの依存．バージョンを指定しないと動かない．
+        # private repositoryの依存がある場合に，探すリンクを書く．バージョンを指定しないと動かない．
         # 'git+https://gitlab.rdcloud.intra.hitachi.co.jp/71389710/python_utility.git#egg=honoka_utility-0.0'
     ],
-    packages=['honoka_utility'],
+    packages=find_packages(),   # __init__.pyのあるディレクトリを探してきて，パッケージに追加．
+    package_data={'honoka_utility': ['data/*']},     # その他，パッケージに含めたいデータを記述．
     zip_safe=False,
 )
